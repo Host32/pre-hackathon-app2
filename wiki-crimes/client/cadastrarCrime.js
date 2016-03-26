0781bfc9efe91d0716d70fd10ba8932b7a0e7480
Maily Santos <mailysantos@gmail.com>
2016-03-26 18:58:19 -0300

    Criando modal para Cadastrar Crimes

--- wiki-crimes/client/cadastrarCrime.js
+++ wiki-crimes/client/cadastrarCrime.js
@@ -1,5 +1,14 @@
-angular.module("wiki-crimes",[]).controller('AppController', ['$scope', '$http', function ($scope, $http) {
+angular.module("wiki-crimes", []).controller('AppController', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.estadoSelecionado = {cidades:[]};
+    $scope.estadoSelecionado = {
+        cidades: []
+    };
+
     $scope.estadosCidades = estadosCidades;
+
+    $scope.abreModal = function () {
+        $('#myModal').modal();
+    };
+
+    $scope.novoCrime = {};
 }]);
