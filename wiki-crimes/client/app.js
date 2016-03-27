0a44583682fb9e04c4461dabf273820899ebd60f
Ivan Miranda <ivan.i.n@hotmail.com>
2016-03-26 22:30:54 -0300

    layout formulario de registro

--- wiki-crimes/client/app.js
+++ wiki-crimes/client/app.js
@@ -1,1 +1,44 @@
-angular.module('wiki-crimes', ['angular-meteor', 'accounts.ui']);
+angular
+    .module('wiki-crimes', ['angular-meteor', 'accounts.ui'])
+    .controller('CrimeController', ['$scope', '$http', function ($scope, $http) {
+
+        $scope.estadoSelecionado = {
+            cidades: []
+        };
+
+        $scope.estadosCidades = estadosCidades;
+
+        $scope.abreModal = function () {
+            $('#myModal').modal();
+        };
+
+        $scope.fechaModal = function () {
+            $('#myModal').modal('hide');
+        }
+
+        $scope.fechaModalErro = function () {
+            $('#modalErro').modal('hide');
+        }
+
+        $scope.fechaModalSucesso = function () {
+            $('#modalSucesso').modal('hide');
+        }
+
+        $scope.novoCrime = {};
+
+        $scope.insereNovoCrime = function (novoCrime) {
+            $scope.fechaModal();
+            Crimes.insert({
+                tipoCrime: novoCrime.tipoCrime,
+                outrosCrimes: novoCrime.outrosCrimes,
+                estado: novoCrime.estadoSelecionado.nome,
+                cidade: novoCrime.cidadeSelecionada,
+                rua: novoCrime.rua,
+                data: novoCrime.data
+            }, function (err) {
+                if (err) {
+                    $('#modalErro').modal();
+                } else $('#modalSucesso').modal();
+            });
+        }
+    }]);
